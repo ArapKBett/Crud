f58cb4768f76214652be8b0ba5e21882d50a257b
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-07-20 01:26:59 +0300

    Create dbconnection.php

---
+++ dbconnection.php
@@ -0,0 +1,13 @@
+<?php
+$db_host = getenv('DB_HOST');
+$db_user = getenv('DB_USER');
+$db_pass = getenv('DB_PASS');
+$db_name = getenv('DB_NAME');
+
+$con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if (mysqli_connect_errno()) {
+    error_log("Database connection failed: " . mysqli_connect_error());
+    die("Connection failed.");
+}
+?>
