a4384bbd08a5cf6ba5390d9b722405e8bd4a8b1a
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-07-20 02:29:50 +0300

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -2,6 +2,9 @@
 $db_host = getenv('DB_HOST');
 $db_user = getenv('DB_USER');
 $db_pass = getenv('DB_PASS');
+if ($db_pass === 'none') {
+    $db_pass = '';
+}
 $db_name = getenv('DB_NAME');
 
 $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
